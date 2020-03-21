3d5d990bd83243770b70629b4b4e5175d6b85149
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2020-03-21 12:53:06 +0430

    Create Navbar Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Header from './Components/Header';
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Header />
       </div>
     );
   }
